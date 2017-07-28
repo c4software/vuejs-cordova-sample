4f3c6b4595e88b84c8a858f3f7886239773e6414
Valentin Brosseau <c4software@gmail.com>
2017-07-28 21:57:40 +0200

    Rename {Hello} => {Home}

---
+++ src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Sample'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: normal;
+  }
+</style>
