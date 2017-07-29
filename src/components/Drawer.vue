fa8af021847ea9b87cdf285b74cb0699f7101840
Valentin Brosseau <c4software@gmail.com>
2017-07-29 17:51:05 +0200

    Create drawer component

---
+++ src/components/Drawer.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <v-toolbar class="primary indigo" />
+    <v-list dense>
+      <template v-for="(item, i) in items">
+        <v-divider dark v-if="item.divider" :key="i"></v-divider>
+        <v-list-tile :key="i" v-else>
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>
+              {{ item.text }}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </template>
+    </v-list>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "drawer",
+  data: function() {
+    return {
+      items: [
+        { icon: 'home', text: 'Home' },
+        { divider: true },
+        { icon: 'help', text: 'See on Github' },
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      msg: 'Sample'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
