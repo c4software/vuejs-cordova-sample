cb53bc5e042890ea3380703f2c3e0dc138f65454
Valentin Brosseau <c4software@gmail.com>
2017-07-29 18:05:26 +0200

    Add VueJS and Cordova Logo in the Home view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="hello">
-    <p>{{ msg }}</p>
+  <div class="home">
+    <img src="../assets/cordova_256.png" />
+    <img src="../assets/vuejs.png" />
+    <h4>{{ msg }}</h4>
   </div>
 </template>
 
@@ -9,11 +11,19 @@ export default {
   name: 'home',
   data () {
     return {
-      msg: 'Sample'
+      msg: 'VueJS Cordova Sample'
     }
   }
 }
 </script>
 
 <style scoped>
+  .home{
+    text-align: center;
+  }
+  .home > img{
+    display: inline;
+    width: 90px;
+    padding: 10px;
+  }
 </style>
