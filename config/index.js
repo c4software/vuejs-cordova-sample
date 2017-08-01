cbb2a63c1ec9a22509b1f36340813c8c7544db2d
Valentin Brosseau <c4software@gmail.com>
2017-08-01 22:16:45 +0200

    Change config, to produce the dist folder directly to the cordova www folder

--- config/index.js
+++ config/index.js
@@ -4,8 +4,8 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    index: path.resolve(__dirname, '../cordova_app/www/index.html'),
+    assetsRoot: path.resolve(__dirname, '../cordova_app/www'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
     productionSourceMap: true,
