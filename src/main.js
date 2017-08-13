5fb3c3d8d898ca0da74a89b79758616f43ef266f
Valentin Brosseau <c4software@gmail.com>
2017-08-13 22:59:15 +0200

    Add missing semicolon

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = true
+Vue.config.productionTip = true;
 
 // Vuetify
 require('vuetify/dist/vuetify.min.css');
