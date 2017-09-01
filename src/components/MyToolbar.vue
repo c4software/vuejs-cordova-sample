8946be1c1dd142a572cd7b5b42e9c7e3c92b35ec
Valentin Brosseau <c4software@gmail.com>
2017-09-01 22:06:42 +0200

    Add a component for the toolbar

---
+++ src/components/MyToolbar.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-navigation-drawer v-model="drawer">
+      <myContentDrawer />
+    </v-navigation-drawer>
+    <v-toolbar class="primary indigo" fixed >
+      <v-toolbar-side-icon dark @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title @click="goHome" class="white--text">{{ $t("title") }}</v-toolbar-title>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+import myContentDrawer from "@/components/Drawer"
+
+export default {
+  name: "myToolbar",
+  components: {myContentDrawer},
+  data: function() {
+    return {
+      drawer: false
+    }
+  },
+  mounted: function () {
+    // Listen for event openDrawer (triggered by other component, like the button in the home)
+    document.addEventListener("toggleDrawer", this.toggleDrawer);
+  },
+  beforeDestroy: function () {
+    // If the component is unmount, unlisten the event.
+    document.removeEventListener("toggleDrawer", this.toggleDrawer);
+  },
+  methods: {
+    toggleDrawer (){
+      this.drawer = !this.drawer;
+    },
+    goHome (){
+      window.location.hash = "/";
+    }
+  }
+}
+</script>
