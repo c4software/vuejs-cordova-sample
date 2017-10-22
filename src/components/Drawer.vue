4786519d70b11a47e47b5e2f439fcd93a4e1da4b
Valentin Brosseau <c4software@gmail.com>
2017-10-22 21:58:42 +0200

    Rewrite menu to use to instead of href to make the menu work again

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -4,7 +4,7 @@
     <v-list dense>
       <template v-for="(item, i) in items">
         <v-divider dark v-if="item.divider" :key="i"></v-divider>
-        <v-list-tile :href="item.action" :key="i" v-else>
+        <v-list-tile :to="item.action" :key="i" v-else>
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-tile-action>
@@ -25,14 +25,12 @@ export default {
   data: function() {
     return {
       items: [
-        { icon: 'home', text: this.$t("drawer.home"), action: '#/' },
-        { icon: 'vibration', text: this.$t('drawer.vibration'), action: '#vibration' },
-        { icon: 'camera', text: this.$t('drawer.camera'), action: '#camera' },
-        { icon: 'flash_on', text: this.$t('drawer.flash'), action: '#flash' },
-        { icon: 'gps_fixed', text: this.$t('drawer.localisation'), action: '#localisation' },
-        { icon: 'nfc', text: this.$t('drawer.nfc'), action: '#nfc' },
-        { divider: true },
-        { icon: 'help', text: this.$t('see_on_github'), action: 'https://github.com/c4software/vuejs-cordova-sample' },
+        { icon: 'home', text: this.$t("drawer.home"), action: '/' },
+        { icon: 'vibration', text: this.$t('drawer.vibration'), action: 'vibration' },
+        { icon: 'camera', text: this.$t('drawer.camera'), action: 'camera' },
+        { icon: 'flash_on', text: this.$t('drawer.flash'), action: 'flash' },
+        { icon: 'gps_fixed', text: this.$t('drawer.localisation'), action: 'localisation' },
+        { icon: 'nfc', text: this.$t('drawer.nfc'), action: 'nfc' }
       ]
     }
   }
