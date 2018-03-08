636e8cbd7a5058629608fe8ebda91b8090e7e248
Valentin Brosseau <c4software@gmail.com>
2018-03-08 21:41:54 +0100

    Use a generic loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-dialog value="true" persistent max-width="290">
+    <v-card>
+      <v-card-text class="text-center">
+        <v-progress-circular :size="70" indeterminate class="primary--text"/>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default{
+    name: 'loading',
+  }
+</script>
